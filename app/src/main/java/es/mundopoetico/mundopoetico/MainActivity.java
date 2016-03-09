59239d33dbdb4f0391ccb0a8ef77ae144c1ce80a
OmegaYalfa <XANDERT2@GMAIL.COM>
2016-03-09 17:24:29 -0600

    APP_MundoPoetico

---
+++ app/src/main/java/es/mundopoetico/mundopoetico/MainActivity.java
@@ -0,0 +1,105 @@
+package es.mundopoetico.mundopoetico;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private AdView mAdView;
+
+    // INI AGREGADO
+    private WebView mWebView;
+    // FIN AGREGADO
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Gets the ad view defined in layout/ad_fragment.xml with ad unit ID set in
+        // values/strings.xml.
+        mAdView = (AdView) findViewById(R.id.ad_view);
+
+        // Create an ad request. Check your logcat output for the hashed device ID to
+        // get test ads on a physical device. e.g.
+        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+
+        // Start loading the ad in the background.
+        mAdView.loadAd(adRequest);
+
+
+
+
+        // INI AGREGADO
+        mWebView = (WebView) findViewById(R.id.activity_main_webview);
+// Activamos javascript
+        WebSettings webSettings = mWebView.getSettings();
+
+// Url que carga la app (webview)
+        mWebView.loadUrl("http://mundopoetico.es");
+// Forzamos el webview para que abra los enlaces internos dentro de la la APP
+        mWebView.setWebViewClient(new WebViewClient());
+// Forzamos el webview para que abra los enlaces externos en el navegador
+        mWebView.setWebViewClient(new MyAppWebViewClient());
+// FIN AGREGADO
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+    // INI AGREGADO
+    @Override
+// Detectar cuando se presiona el botón de retroceso
+    public void onBackPressed() {
+        if(mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+    // FIN AGREGADO
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
